abb781ac12c50b190bf2825a41ee661aaeb3c3ef
Kert <yang.luo@hp.com>
2016-05-23 17:28:25 +0800

    more js files.

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,17 @@
-app = angular.module('MySimpleEditor', ['textAngular']); 
+/*app = angular.module('MySimpleEditor', ['textAngular']); 
 
 app.controller('MyController', ['$scope', function($scope){
     $scope.sample = 'Shout out from the javascript!';
     $scope.htmlContent = '<h2>Testing Content</h2>';
-}]);
+    }]);*/
+
+(function() {
+	  angular
+		    .module("MySimpleEditor", ['textAngular'])
+		    .controller("MyController", MyController);
+    
+
+	  function MyController($scope) {
+		    $scope.sample='abcdefg';
+	  };
+})();
